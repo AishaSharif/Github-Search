e43baba31c93027919d921945883880c65f738c3
Aisha Sharif <sharif_aisha@yahoo.com>
2019-05-05 15:16:54 +0300

    Add UserServices service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment} from '../../environments/environment'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  repoUrl = 'https://api.github.com/search/repositories?q='
+  userUrl = 'https://api.github.com/users/'
+  accessToken = environment.accessToken
+  repoName:string
+  userName: string
+  // repo:string
+  // repos:any
+  // users: any
+  
+  constructor(private http:HttpClient) { }
+
+  searchRepo(repoName){
+    let searchRepoLink = this.repoUrl + this.repoName
+    this.repoName = repoName
+    return this.http.get(searchRepoLink)
+  }
+
+  getUser(userName){
+    let userLink = this.userUrl + this.userName 
+    this.userName = userName
+    return this.http.get(userLink)
+  }
+  getRepo(userName){
+    this.userName = userName
+    let repoLink = this.userUrl + this.userName + '/repos'
+    return this.http.get(repoLink)
+  }
+}
