29a91b4e9d6834599c7138e83483210ba63f2bf9
Aisha Sharif <sharif_aisha@yahoo.com>
2019-05-06 08:26:26 +0300

    Configure routing

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule,Routes} from '@angular/router';
+
+import { UserComponent } from '../user/user.component';
+import { ReposComponent }  from '../repos/repos.component'
+import { NotFoundComponent } from '../not-found/not-found.component'
+
+const routes:Routes=[
+  {path:'user', component:UserComponent},
+  // {path:'', redirectTo:'/users', pathMatch:'full'},
+  {path:'repos', component:ReposComponent},
+  {path:'**', component:NotFoundComponent}
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
 })
 export class RoutingModule { }
